fd481441a9fc446f6bf56510e63c25b85989040d
ajb <>
2024-01-22 09:28:26 -0600

    sort and search

--- sort-and-search/sas.go
+++ sort-and-search/sas.go
@@ -45,6 +45,41 @@ func bubbleSort(slice []int) {
 	}
 }
 
+func partition(slice []int) int {
+	lo := 0
+	hi := len(slice)-1
+	
+	pivot := slice[hi]
+
+	i := lo - 1
+
+	for j := lo; j < hi; j++ {
+		if slice[j] <= pivot {
+			i = i + 1
+			slice[i], slice[j] = slice[j], slice[i]
+		}
+	}
+
+	i = i + 1
+	slice[i], slice[hi] = slice[hi], slice[i]
+
+	return i
+}
+
+func quicksort(slice []int) {
+	if len(slice) < 2 {
+		return
+	}
+	
+	p := partition(slice)
+
+	quicksort(slice[0:p])
+	quicksort(slice[p+1:])
+}
+
+
+/*
+// bubble sort
 func main() {
     // Get the number of items and maximum item value.
     var numItems, max int
@@ -65,3 +100,27 @@ func main() {
     // Verify that it's sorted.
     checkSorted(slice)
 }
+*/   
+
+
+func main() {
+    // Get the number of items and maximum item value.
+    var numItems, max int;
+    fmt.Printf("# Items: ")
+    fmt.Scanln(&numItems)
+    fmt.Printf("Max: ")
+    fmt.Scanln(&max)
+
+    // Make and display the unsorted slice.
+    slice := makeRandomSlice(numItems, max)
+    printSlice(slice, 40)
+    fmt.Println()
+
+    // Sort and display the result.
+    quicksort(slice)
+    printSlice(slice, 40)
+
+    // Verify that it's sorted.
+    checkSorted(slice)
+}
+
